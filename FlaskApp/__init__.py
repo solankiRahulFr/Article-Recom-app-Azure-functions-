5827228cb54c0e19cb214957c9797b5052a4a3e2
solankiRahulFr <99829859+solankiRahulFr@users.noreply.github.com>
2023-07-25 17:03:14 +0200

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -8,12 +8,12 @@ from .package.module import MODULE_VALUE
 
 app = Flask(__name__)
 rootPath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-def loadModules():
-    load_model = pickle.load(open(rootPath + '/FlaskApp/app_modules/lightfm_model_hybrid.pkl','rb'))
-    load_interactions = pickle.load(open(rootPath + '/FlaskApp/app_modules/interactions.pkl','rb'))
-    load_item_features_matrix = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_features_matrix.pkl','rb'))
-    load_item_dict = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_dict.pkl','rb'))
-    return load_model, load_interactions, load_item_features_matrix, load_item_dict
+
+
+load_model = pickle.load(open(rootPath + '/FlaskApp/app_modules/lightfm_model_hybrid.pkl','rb'))
+load_interactions = pickle.load(open(rootPath + '/FlaskApp/app_modules/interactions.pkl','rb'))
+load_item_features_matrix = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_features_matrix.pkl','rb'))
+load_item_dict = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_dict.pkl','rb'))
 
 
 
@@ -43,8 +43,7 @@ def n_recommendation(model, interactions, user_id, item_dict , item_features_mat
 @app.route("/predictArticles/<id>", methods=['GET'])
 def predictArticles(id: int):
     userid = int(id)
-    model, interactions, item_features_matrix, item_dict = loadModules()
-    recom_articles, recom_categories=n_recommendation(model, interactions, userid, item_dict, item_features_matrix, 5)
+    recom_articles, recom_categories=n_recommendation(load_model, load_interactions, userid, load_item_dict, load_item_features_matrix, 5)
     return jsonify(userid = userid,
                    articles=','.join(str(v) for v in recom_articles),
                    categories=','.join(str(v) for v in set(recom_categories)))
