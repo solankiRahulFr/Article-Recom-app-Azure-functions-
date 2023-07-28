67e79b21981ab1f0d35b466dc004bcb361bb5335
solankiRahulFr <99829859+solankiRahulFr@users.noreply.github.com>
2023-07-28 23:41:53 +0200

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -9,10 +9,12 @@ from azure.storage.blob import BlobClient
 
 app = Flask(__name__)
 rootPath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+connection_string = "DefaultEndpointsProtocol=https;AccountName=project09group8aa0;AccountKey=8P39lFEsZcCnUDQe8b0cg4izw4JCp1kiVETWh6/sm/WEFKgNYQsD3HALdtZSN+C640t8ufQKGLna+AStEbySIg==;EndpointSuffix=core.windows.net"
+
 
 def read_pickle_from_azure(blob_name):
     blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="picklefiles", blob_name)
-    
+    print("Downloading..."+blob_name)
     with open("models/"+blob_name, "wb") as my_blob:
         blob_data = blob.download_blob()
         blob_data.readinto(my_blob)
