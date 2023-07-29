2fed20d81dbbf0929bb67177897b7ed9099a353e
solankiRahulFr <99829859+solankiRahulFr@users.noreply.github.com>
2023-07-29 03:13:11 +0200

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -5,31 +5,19 @@ import os
 import lightfm
 # Always use relative import for custom module
 from .package.module import MODULE_VALUE
-from azure.storage.blob import BlobClient
+# from azure.storage.blob import BlobClient
 
 app = Flask(__name__)
 rootPath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-connection_string = "DefaultEndpointsProtocol=https;AccountName=project09group8aa0;AccountKey=8P39lFEsZcCnUDQe8b0cg4izw4JCp1kiVETWh6/sm/WEFKgNYQsD3HALdtZSN+C640t8ufQKGLna+AStEbySIg==;EndpointSuffix=core.windows.net"
 
-
-def read_pickle_from_azure(blob_name):
-    blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="picklefiles", blob_name)
-    print("Downloading..."+blob_name)
-    with open("models/"+blob_name, "wb") as my_blob:
-        blob_data = blob.download_blob()
-        blob_data.readinto(my_blob)
 
 
 def loadModule():
-    # global load_model, load_interactions, load_item_features_matrix, load_item_dict
-    read_pickle_from_azure("lightfm_model_hybrid.pkl")
-    read_pickle_from_azure("interactions.pkl")
-    read_pickle_from_azure("item_features_matrix.pkl")
-    read_pickle_from_azure("item_dict.pkl")
-    # load_model = pickle.load(open(rootPath + '/FlaskApp/app_modules/lightfm_model_hybrid.pkl','rb'))
-    # load_interactions = pickle.load(open(rootPath + '/FlaskApp/app_modules/interactions.pkl','rb'))
-    # load_item_features_matrix = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_features_matrix.pkl','rb'))
-    # load_item_dict = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_dict.pkl','rb'))
+    global load_model, load_interactions, load_item_features_matrix, load_item_dict
+    load_model = pickle.load(open(rootPath + '/FlaskApp/app_modules/lightfm_model_hybrid.pkl','rb'))
+    load_interactions = pickle.load(open(rootPath + '/FlaskApp/app_modules/interactions.pkl','rb'))
+    load_item_features_matrix = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_features_matrix.pkl','rb'))
+    load_item_dict = pickle.load(open(rootPath + '/FlaskApp/app_modules/item_dict.pkl','rb'))
 
 
 
@@ -57,7 +45,7 @@ def n_recommendation(model, interactions, user_id, item_dict , item_features_mat
 
 @app.route("/")
 def index():
-    check = os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/FlaskApp/app_modules/lightfm_model_hybrid.pkl')
+    check = os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/models/lightfm_model_hybrid.pkl')
     return jsonify(check=check)
 
 @app.route("/predictArticles/<id>", methods=['GET'])
